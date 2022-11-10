61fbcbd002121c3095328a1d3a8db683502e18b2
yshwaker <yshwaker@gmail.com>
2022-11-10 10:06:50 +0800

    threejs journey physics

--- threejs-journey/22-physics/src/script.js
+++ threejs-journey/22-physics/src/script.js
@@ -8,6 +8,35 @@ import './style.css'
  * Debug
  */
 const gui = new dat.GUI()
+const debugObject = {
+  createSphere: () => {
+    createSphere(Math.random() / 2, {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    })
+  },
+  createBox: () => {
+    createBox(Math.random(), Math.random(), Math.random(), {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    })
+  },
+  reset: () => {
+    for (const object of objectsToUpdate) {
+      object.body.removeEventListener('collide', playHitSound)
+      world.removeBody(object.body)
+
+      scene.remove(object.mesh)
+    }
+
+    objectsToUpdate.splice(0, objectsToUpdate.length)
+  },
+}
+gui.add(debugObject, 'createSphere')
+gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
 
 /**
  * Base
@@ -34,11 +63,46 @@ const environmentMapTexture = cubeTextureLoader.load([
 ])
 
 /**
+ * Sound
+ */
+const hitSound = new Audio('/sounds/hit.mp3')
+
+let debounce
+const playHitSound = (collision) => {
+  const impactStrength = collision.contact.getImpactVelocityAlongNormal()
+
+  if (impactStrength > 1.5) {
+    hitSound.volume = Math.min(1, impactStrength / 10)
+    hitSound.currentTime = 0
+    clearTimeout(debounce)
+    debounce = setTimeout(() => {
+      hitSound.play()
+    }, 100)
+  }
+}
+
+/**
  * Physics
  */
 // World
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
 world.gravity.set(0, -9.82, 0)
+
+// Materials
+const defaultMaterial = new CANNON.Material('default')
+
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+)
+
+world.addContactMaterial(defaultContactMaterial)
 
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5)
@@ -46,25 +110,20 @@ const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: defaultMaterial,
 })
-
+sphereBody.applyLocalForce(new CANNON.Vec3(100, 0, 0), new CANNON.Vec3(0, 0, 0))
 world.addBody(sphereBody)
 
-/**
- * Test sphere
- */
-const sphere = new THREE.Mesh(
-  new THREE.SphereGeometry(0.5, 32, 32),
-  new THREE.MeshStandardMaterial({
-    metalness: 0.3,
-    roughness: 0.4,
-    envMap: environmentMapTexture,
-    envMapIntensity: 0.5,
-  })
-)
-sphere.castShadow = true
-sphere.position.y = 0.5
-scene.add(sphere)
+// Floor
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+floorBody.mass = 0 // don't let it fall
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
+floorBody.material = defaultMaterial
+
+world.addBody(floorBody)
 
 /**
  * Floor
@@ -151,25 +210,105 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Utils
+ */
+
+const objectsToUpdate = []
+const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5,
+})
+const createSphere = (radius, position) => {
+  // Threejs mesh
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+  mesh.scale.set(radius, radius, radius)
+  mesh.castShadow = true
+  mesh.position.copy(position)
+  scene.add(mesh)
+
+  // Physics Body
+  const shape = new CANNON.Sphere(radius)
+  const body = new CANNON.Body({
+    mass: 1,
+    shape,
+    position,
+    material: defaultMaterial,
+  })
+  body.addEventListener('collide', playHitSound)
+  world.addBody(body)
+
+  objectsToUpdate.push({
+    mesh,
+    body,
+  })
+}
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.1,
+  roughness: 0.8,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5,
+})
+
+const createBox = (width, height, depth, position) => {
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial)
+  mesh.scale.set(width, height, depth)
+  mesh.castShadow = true
+  mesh.position.set(position)
+  scene.add(mesh)
+
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width / 2, height / 2, depth / 2)
+  )
+  const body = new CANNON.Body({
+    position,
+    mass: 10,
+    material: defaultMaterial,
+    shape,
+  })
+
+  body.addEventListener('collide', playHitSound)
+
+  world.addBody(body)
+
+  objectsToUpdate.push({
+    mesh,
+    body,
+  })
+}
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
 let oldTime = 0
 
 const tick = () => {
-  // const elapsedTime = clock.getElapsedTime()
+  const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - oldTime
   oldTime = elapsedTime
 
   // Update physics world
+
+  // sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position)
+
   /**
    * params:
    * 1. a fixed time step. we want to run at 60 fps. so each step is 1/60 s
    * 2. how much time passed since the last step.
-   * 3.
+   * 3. how much step at most to catch up
    */
   // https://gafferongames.com/post/fix_your_timestep/
   world.step(1 / 60, deltaTime, 3)
+
+  for (const object of objectsToUpdate) {
+    object.mesh.position.copy(object.body.position)
+    object.mesh.quaternion.copy(object.body.quaternion)
+  }
 
   // Update controls
   controls.update()
