bc888bd2735e81771893adb4ca36aa56d20bc90e
yshwaker <yshwaker@gmail.com>
2022-11-25 15:00:36 +0800

    lesson 43

---
+++ threejs-journey/43-r3f-drei/src/Experience.js
@@ -0,0 +1,75 @@
+import {
+  Float,
+  Html,
+  MeshReflectorMaterial,
+  OrbitControls,
+  PivotControls,
+  Text,
+  TransformControls,
+} from '@react-three/drei'
+import { useRef } from 'react'
+
+export default function Experience() {
+  const cube = useRef()
+  const sphere = useRef()
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <PivotControls
+        anchor={[0, 0, 0]}
+        depthTest={false}
+        lineWidth={4}
+        axisColors={['#9381ff', '#994d4d', '#7ae582']}
+        scale={100}
+        fixed={true}
+      >
+        <mesh ref={sphere} position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+          <Html
+            position={[1, 1, 0]}
+            wrapperClass="label"
+            center
+            distanceFactor={6}
+            occlude={[cube, sphere]}
+          >
+            Pivot
+          </Html>
+        </mesh>
+      </PivotControls>
+
+      <mesh ref={cube} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <TransformControls object={cube} />
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        {/* <meshStandardMaterial color="greenyellow" /> */}
+        <MeshReflectorMaterial
+          resolution={512}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          mirror={0.7}
+        />
+      </mesh>
+
+      <Float>
+        <Text
+          font="./bangers-v20-latin-regular.woff"
+          color="#ff0000"
+          fontSize={1}
+          position={[0, 2, 0]}
+        >
+          Learning Threejs
+        </Text>
+      </Float>
+    </>
+  )
+}
